b880624cedb77c4e0231d897ce0ec71c6b463356
Aditya <aditya.gaonkar1907@gmail.com>
2026-06-20 16:59:23 +0530

    DEBUG added

--- server/src/config/socket.js
+++ server/src/config/socket.js
@@ -9,6 +9,12 @@ export function initializeSocket(server) {
   });
 
   io.on("connection", (socket) => {
+    console.log("Client Connected:", socket.id);
+
+    socket.on("disconnect", () => {
+      console.log("Client Disconnected:", socket.id);
+    });
+
     signalingHandler(io, socket);
   });
 
